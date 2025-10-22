df92aa51aa5767cb11f70c0b64740597947e2873
Vuk-afk <vuk.vukovic111@gmail.com>
2025-10-22 12:05:39 +0200

    sdsd

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title>Brukerfunksjoner</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav aria-label="Hovedmeny">
+            <ul>
+                <li><a href="Registrer_klasse.php">Registrer klasse</a></li>
+                <li><a href="Vis_alle_klasser.php">Gruppekontrakt</a></li>
+                <li><a href="html-css.html">HTML/CSS</a></li>
+                <li><a href="personas.html">Personas</a></li>
+                <li><a href="ia-wireframes.html">IA & Wireframes</a></li>
+                <li><a href="universell.html">Universell utforming</a></li>
+                <li><a href="brukerhistorier.html">Brukerhistorier</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h2>Velkommen til vÃ¥rt prosjekt</h2>
+        <p>Denne nettsiden er laget som en del av gruppeoppgaven. 
+           Her finner du vÃ¥re ulike leveranser som egne undersider.</p>
+    </main>
+    <footer>
+        <p>Vuk</p>
+    </footer>
+</body>
+</html>
